45b5f088bcafc22d7d65de070f1df7bf62b08d6c
FranciscoRam <r.g.francisco40@gmail.com>
2019-05-26 21:57:34 -0500

    CRUD fields and relation

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -1,4 +1,45 @@
 class FieldsController < ApplicationController
+    before_action :set_user 
+    def new
+        @field = Field.new
+    end
+    def create
+        @field = current_user.fields.new field_params
+        if @field.save
+            return redirect_to @field
+        end
+        render 'new'
+    end
+    def show
+        @field = Field.where({user_id: current_user.id, id: params[:id]})
+    end
     def index
+        @fields = Field.where user_id: current_user.id
+    end
+    def edit
+        @field = Field.find params[:id]
+    end
+    def update
+        @field = Field.find params[:id]
+        if @field.update field_params
+            return redirect_to @field
+        end
+        render 'edit'
+    end
+    def destroy
+        @field = Field.find params[:id]
+        @field.destroy
+        redirect_to fields_path
+    end
+
+    private 
+    def field_params 
+        params.require(:field).permit :description, :token, :lenguage
+    end
+
+    def set_user 
+        if !(current_user)
+            redirect_to ""
+        end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :publications #Le dice que tiene una relacion a muchos datos de publications
   has_many :data_users
+  has_many :fields
 end
